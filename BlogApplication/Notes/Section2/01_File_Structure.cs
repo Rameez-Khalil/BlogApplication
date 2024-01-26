2b80548064e6fdac3d1a63967df59d6e24bbd28e
rameez <rameezkhalil23@gmail.com>
2024-01-25 16:22:54 -0800

    section2

---
+++ BlogApplication/Notes/Section2/01_File_Structure.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * SOLUTION - Architecture for organizing project.
+ * 
+ * LAUNCH SETTINGS - This contains information for runnable profiles.
+ * 
+ * ROOT - Static assets being served to the client.
+ * 
+ * CONTROLLERS - Handlers requests, contains class files.
+ * 
+ * MODELS - Model class files.
+ * 
+ * VIEWS - HTML file for our application.
+ *          Each controller will have its own view.
+ *          
+ * SHARED - Shared views which will be shared among 
+ *          all controllers.
+ *          
+ * APP SETTINGS - All the app settings.
+ *                This stores database information.
+ *                
+ * PROGRAM CS - Entry point.
+ *              Contains requets pipeline.
+ *              
+ * 
+ */
---
+++ BlogApplication/Notes/Section2/02_Creating_Domain_Models.cs
@@ -0,0 +1,8 @@
+﻿/*
+ * ADMIN has the functionality of creating a blogpost and assign tags into it.
+ * 
+ * A single tag can be used to index multiple tags.
+ * 
+ * One BlogPosts can have many tags, and multiple tags can have many blogposts -> Many To Many Relationship.
+ * 
+ */
