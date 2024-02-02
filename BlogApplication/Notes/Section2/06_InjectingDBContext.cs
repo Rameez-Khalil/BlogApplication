6b687131193a2490c17bf63ef1e0942019daee25
rameez <rameezkhalil23@gmail.com>
2024-02-01 17:01:42 -0800

    Change in nots

--- BlogApplication/Notes/Section2/06_InjectingDBContext.cs
+++ BlogApplication/Notes/Section2/06_InjectingDBContext.cs
@@ -7,4 +7,13 @@
  *      BUILDER : 
  *             1. builder is an object coming from WebApplicationBuilder
  *             2. builder.services.AddDbContext<DbContextType> 
+ *             //We will pass the options from our DBContext class
+                //builder object will fetch the name of provided connection string
+                builder.Services.AddDbContext<BloggieDbContext>(options =>
+                    options.UseSqlServer(builder.Configuration.GetConnectionString("BloggieDbConnection")));
+ *             
+ *     DEPENDENCY INJECTION:
+ *          Dependency Injection is all about giving an object its dependency without letting the receiving end constructing it.
+ *          
+ *          
  */
